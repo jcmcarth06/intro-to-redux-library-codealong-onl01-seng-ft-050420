3196fe2c9fba1449d69759d8423b261f9d9e2927
Joshua McArthur <jcmcarth06@gmail.com>
2020-09-18 01:57:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = reateStore(shoppingListItemReducer);
+const store = createStore(shoppingListItemReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
